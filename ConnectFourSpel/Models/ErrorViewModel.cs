095634bb26280e47a7fd11aa254f244880976d7b
kaosanova <christofferedl@hotmail.com>
2025-10-21 15:21:06 +0200

    Projektstart

---
+++ ConnectFourSpel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConnectFourSpel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
