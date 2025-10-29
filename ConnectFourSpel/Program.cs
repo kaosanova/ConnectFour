5a1b7f74567b44522416d0b79c8212b4b9b57f8b
kaosanova <christofferedl@hotmail.com>
2025-10-29 12:15:03 +0100

    Databaslösning

--- ConnectFourSpel/Program.cs
+++ ConnectFourSpel/Program.cs
@@ -1,5 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using ConnectFourSpel.Services;
-using Microsoft.AspNetCore.Connections;
 
 
 
@@ -10,10 +10,29 @@ builder.Services.AddControllersWithViews();
 
 // Vårt in-memory-lager för spel (DI)
 builder.Services.AddSingleton<IGameStore, InMemoryGameStore>();
-
 builder.Services.AddSingleton<IDbConnectionFactory, SqlConnectionFactory>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(o =>
+    {
+        o.LoginPath = "/Auth/Login";
+        o.LogoutPath = "/Auth/Logout";
+        o.Cookie.Name = "connect4.auth";
+        o.Cookie.HttpOnly = true;
+        o.SlidingExpiration = true;
+        // sätt o.Cookie.SecurePolicy = CookieSecurePolicy.Always vid HTTPS/produktion
+        o.Cookie.SameSite = SameSiteMode.Lax;
+    });
+
+
+builder.Services.AddSession(o =>
+{
+    o.IdleTimeout = TimeSpan.FromHours(8);
+    o.Cookie.Name = "connect4.session";
+    o.Cookie.HttpOnly = true;
+    o.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -27,8 +46,12 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
+app.UseSession();               
+app.UseAuthentication();       
 app.UseAuthorization();
+
+app.UseRouting();
+
 
 // Standardroute: /{controller}/{action}/{id?}
 app.MapControllerRoute(
